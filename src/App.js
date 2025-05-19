29cfe1dd129ab691655fe84a1b12330d3b899bd5
Ruhshona <abduvaxobova.r@gmail.com>
2025-05-19 19:09:00 +0500

    Website Pizza is finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import './App.css'
 
 function App() {
@@ -10,7 +14,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer/>
     </Router>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/pizzaLogo.png'; 
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css';
 
-
 function Navbar() {
   const [openLinks, setOpenLinks] = useState(false);
 
-  const toggleNavbar = () =>{
+  const toggleNavbar = () => {
     setOpenLinks(!openLinks);
-  }
+  };
+
   return (
     <div className="navbar">
-      <div className='leftSide' id={openLinks ? "open" : "close"}>
+      <div className='leftSide'>
         <img src={Logo} alt="logo" />
-          <div className='hiddenLinks'>
-              <Link to="/">Home</Link>
-              <Link to="/menu">Menu</Link>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
-          </div>
       </div>
+
       <div className='rightSide'>
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
         <button onClick={toggleNavbar}>
-          <ReorderIcon/>
+          <ReorderIcon />
         </button>
+      </div>
+
+      <div className={openLinks ? "hiddenLinks open" : "hiddenLinks"}>
+        <Link to="/" onClick={() => setOpenLinks(false)}>Home</Link>
+        <Link to="/menu" onClick={() => setOpenLinks(false)}>Menu</Link>
+        <Link to="/about" onClick={() => setOpenLinks(false)}>About</Link>
+        <Link to="/contact" onClick={() => setOpenLinks(false)}>Contact</Link>
       </div>
     </div>
   );
